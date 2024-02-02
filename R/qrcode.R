26eaeecc930968cd22a8a4bc245c1481970c5976
Caio <caio.azevedo@live.com>
2024-02-02 16:36:36 -0300

    fig

--- R/qrcode.R
+++ R/qrcode.R
@@ -1,8 +1,9 @@
 library(ggplot2)
 url <- "https://caio-azevedo.github.io/oficina_futebol"
 
-qrcode <- plot(qrcode::qr_code(url, ecl = 'M'), col = c('white', '#261373'))
+plot(qrcode::qr_code(url, ecl = 'M'),
+               col = c('white', '#261373'))
 
-ggsave("qrcode.png", plot = qrcode,
-       width = 6, height = 6, dpi = 300)
+dev.copy(png,"qrcode.png")
+dev.off()
 
